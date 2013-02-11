4e30f207d63f44faa6ab099385d790a04985f6ad
rlbisbe <roberto.luis@rlbisbe.net>
2013-02-11 21:22:10 +0100

    Primera ronda

---
+++ 12Enero_CodeBreaker/CodeMachine.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12Enero_CodeBreaker
+{
+    public class CodeMachine
+    {
+        private string password;
+
+        public CodeMachine(string password)
+        {
+            this.password = password;
+        }
+
+        public string Decode(string p)
+        {
+            List<char> exactPositions = new List<char>();
+            List<char> unexactPositions = new List<char>();
+
+            char[] array = p.ToCharArray();
+            char[] passwordToDecode = password.ToCharArray();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == passwordToDecode[i])
+                {
+                    exactPositions.Add('X');
+                    continue;
+                }
+                if (passwordToDecode.Contains(array[i]))
+                    unexactPositions.Add('*');
+            }
+
+            exactPositions.AddRange(unexactPositions);
+
+            return String.Concat(exactPositions);
+        }
+    }
+}
---
+++ Tests/CodeMachineTester.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _12Enero_CodeBreaker;
+
+namespace Tests
+{
+    [TestClass]
+    public class CodeMachineTester
+    {
+        [TestMethod]
+        public void BasicReturnShouldBeAValidString()
+        {
+            CodeMachine machine = new CodeMachine(string.Empty);
+            Assert.IsNotNull(machine.Decode(""));
+        }
+
+        [TestMethod]
+        public void CorrectCodeShouldHaveACorrectAnswer()
+        {
+            CodeMachine machine = new CodeMachine("AAAA");
+            Assert.AreEqual(machine.Decode("AAAA"), "XXXX");
+        }
+
+        [TestMethod]
+        public void AlmostCorrectCodeShouldHaveAnAlmostCorrectAnswer()
+        {
+            CodeMachine machine = new CodeMachine("AAAA");
+            Assert.AreEqual(machine.Decode("AABA"), "XXX");
+            Assert.AreEqual(machine.Decode("AAAB"), "XXX");
+            Assert.AreEqual(machine.Decode("ABAA"), "XXX");
+            Assert.AreEqual(machine.Decode("BAAA"), "XXX");
+        }
+
+        [TestMethod]
+        public void UnorderedCodeShouldHaveXMarks()
+        {
+            CodeMachine machine = new CodeMachine("ABCD");
+            Assert.AreEqual(machine.Decode("BCDA"), "****");
+            Assert.AreEqual(machine.Decode("BCD"), "***");
+        }
+
+        [TestMethod]
+        public void PasswordShouldHaveNoMoreThanFourCharacters()
+        {
+            CodeMachine machine = new CodeMachine("KKKK");
+        }
+    }
+}
